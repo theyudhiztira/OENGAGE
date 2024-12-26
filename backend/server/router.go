68d6e8bf466eecc2ad9dc132e5bcc6974cf565c1
Pandu Yudhistira <theyudhztira@gmail.com>
2024-12-27 00:06:00 +0700

    Handle CORS

--- backend/server/router.go
+++ backend/server/router.go
@@ -3,13 +3,24 @@ package server
 import (
 	"context"
 	"theyudhiztira/oengage-backend/route"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func Router(db *mongo.Database, ctx *context.Context) *gin.Engine {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	routerv1 := router.Group("/v1")
 	{
